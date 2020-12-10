fb447a73b8191021698e65d6066dee98fe99b55d
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2020-12-10 10:20:13 +0000

    Refactored App.js, added UserProvider

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+// import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import UserProvider from './src/modules/Users/store/UserContext';
+import AppNavigation from './src/navigation';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <UserProvider>
+            <AppNavigation />
+        </UserProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
