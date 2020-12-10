d73b4dada6e3ba3656edd89a6786fd404033069d
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2020-12-10 10:22:24 +0000

    Added global styles and colors

---
+++ src/globals/Styles.js
@@ -0,0 +1,9 @@
+export default {
+    shadow: {
+        elevation: 5,
+        shadowOpacity: 0.7,
+        shadowColor: '#000',
+        position: 'relative',
+        shadowOffset: {width: 0, height: 0},
+    },
+};
---
+++ src/globals/index.js
@@ -0,0 +1,6 @@
+import Colors from './Colors';
+import Constants from './constants';
+import Helpers from './Helpers';
+import Styles from './Styles';
+
+export {Colors, Constants, Helpers, Styles};
