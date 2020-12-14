90936873ab346948f16dd7f75cccd5391f1aeb41
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2020-12-14 09:26:35 +0000

    Adding action creator for authenticating user

--- src/modules/Users/store/actions.js
+++ src/modules/Users/store/actions.js
@@ -3,7 +3,7 @@ import * as types from './types';
 /**
  * Action creator for handling email
  *
- * @param {Object} dispatch
+ * @param {Function} dispatch
  * @param {String} payload
  */
 export const emailChanged = (dispatch, payload) => {
@@ -13,9 +13,26 @@ export const emailChanged = (dispatch, payload) => {
 /**
  * Action creator for handling password
  *
- * @param {Object} dispatch
+ * @param {Function} dispatch
  * @param {String} payload
  */
 export const passwordChanged = (dispatch, payload) => {
     dispatch({type: types.PASSWORD_CHANGED, payload});
+};
+
+/**
+ * Action creator for authenticating user
+ *
+ * @param {Function} dispatch
+ * @param {Object} data
+ */
+export const authenticateUser = async (dispatch, data) => {
+    try {
+        dispatch({type: types.AUTHENTICATE_USER_START});
+
+        dispatch({type: types.AUTHENTICATE_USER_SUCCESS, payload: null});
+    } catch (error) {
+        dispatch({type: types.AUTHENTICATE_USER_FAIL, payload: error});
+        console.log('User Action Creator, Error authenticating user: ', error);
+    }
 };
