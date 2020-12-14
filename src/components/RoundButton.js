733b27369136002be5aeaab3574c9bcdff4a7ecd
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2020-12-14 09:23:21 +0000

    Added Icon to RoundButton component.

--- src/components/RoundButton.js
+++ src/components/RoundButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Pressable, Text} from 'react-native';
 import {Styles} from './../globals';
 
-const RoundButton = ({title, onPress, style, backgroundColor="#FFF", color="#000", fontSize=16, shadow=false}) => {
+const RoundButton = ({title, onPress, style, backgroundColor="#FFF", color="#000", fontSize=16, shadow=false, IconFamily, iconName, iconSize=18}) => {
     let shadowStyles = shadow ? Styles.shadow : null;
 
     let styles = {
@@ -10,13 +10,17 @@ const RoundButton = ({title, onPress, style, backgroundColor="#FFF", color="#000
         paddingHorizontal: 20,
         paddingVertical: 10,
         borderRadius: 30,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'center',
         ...style,
         ...shadowStyles,
     };
 
     return (
         <Pressable onPress={onPress} style={styles}>
-            <Text style={{color, fontSize, textAlign: 'center'}}>{title}</Text>
+            <IconFamily name={iconName} size={iconSize} color={color} />
+            <Text style={{color, fontSize, textAlign: 'center', marginLeft: 10}}>{title}</Text>
         </Pressable>
     );
 };
