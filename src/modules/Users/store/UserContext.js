8a1e17775e0c7d3c4a5bee190f143a33d3d9c814
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2020-12-14 09:28:30 +0000

    Refactoring User Context

--- src/modules/Users/store/UserContext.js
+++ src/modules/Users/store/UserContext.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import reducer from './reducer';
+import userReducer from './reducer';
+
+let initialState = {
+    users: [],
+    email: '',
+    user: null,
+    password: '',
+    is_authenticated: false,
+    authenticating_user: false,
+    authenticating_user_error: '',
+};
 
 export const UserContext = React.createContext();
 
 const UserProvider = ({children}) => {
-    let initialState = {
-        email: 'godfred@gmail.com',
-        password: '',
-        users: [],
-    };
-
-    const [userState, dispatch] = React.useReducer(reducer, initialState);
+    const [userState, dispatch] = React.useReducer(userReducer, initialState);
 
     return (
         <UserContext.Provider value={{userState, dispatch}}>
             {children}
         </UserContext.Provider>
     );
-}
+};
 
 export default UserProvider;
