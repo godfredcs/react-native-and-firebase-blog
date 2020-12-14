0cfc3d54c22ee3aecbbbc47ffbd8a5ea20c5dd48
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2020-12-14 09:27:14 +0000

    Handling authentication in user reducer

--- src/modules/Users/store/reducer.js
+++ src/modules/Users/store/reducer.js
@@ -14,6 +14,30 @@ export default (state, action) => {
                 password: action.payload,
             };
 
+        case types.AUTHENTICATE_USER_START:
+            return {
+                ...state,
+                authenticating_user: true,
+                authenticating_user_error: '',
+            };
+
+        case types.AUTHENTICATE_USER_SUCCESS:
+            return {
+                ...state,
+                user: action.payload,
+                is_authenticated: true,
+                authenticating_user: false,
+            };
+
+        case types.AUTHENTICATE_USER_FAIL:
+            return {
+                ...state,
+                user: null,
+                is_authenticated: false,
+                authenticating_user: false,
+                authenticating_user_error: action.payload,
+            };
+
         default:
             throw new Error(`Users Reducer Unhandled Action Type: ${action.type}`);
     }
